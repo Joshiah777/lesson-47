05050dbde62cd6fb1a1383566537c4257e7556cc
Joshiah777 <Joshiahpthomas@gmail.com>
2025-04-19 14:16:22 +0530

    demo

---
+++ month.py
@@ -0,0 +1,21 @@
+import.calender
+print ("January")
+print ("February") 
+print ("March")
+print ("April")
+print ("May")
+print ("June")
+print ("July")
+print ("August")
+print ("September") 
+print ("October")
+print ("November")
+print ("December")
+
+
+
+
+
+
+
+
